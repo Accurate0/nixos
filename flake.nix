32118263d8403a2fc018a4d8a115e11aec9abb42
Anurag Singh <contact@anurag.sh>
2023-02-06 23:08:51 +0800

    use release for home manager + upgrade

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs";
     home-manager.url = "github:nix-community/home-manager/release-22.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
